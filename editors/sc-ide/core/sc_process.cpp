8d071baa3ac306af4a0267a4d24631a66efd48ee
Miguel Negrao <miguel.negrao@friendlyvirus.org>
2017-06-23 14:28:32 +0100

    ide: implements projects
    File open, save, etc menu actions for projects.
    Adds project editing preference widget.
    No longer saves "standalone" option in ide preferences, instead display the 'excludeDefaultPaths' option in 'interpreter' pane of the preferences dialog.
    ide can be in two different modes, normal or project, this is decided by the '"IDE/interpreter/project' boolean key in the ide preferences. This key is set to true when opening a project and set to false and closing the project.

--- editors/sc-ide/core/sc_process.cpp
+++ editors/sc-ide/core/sc_process.cpp
@@ -115,6 +115,11 @@ void ScProcess::updateToggleRunningAction()
     mActions[ToggleRunning]->setShortcut( targetAction->shortcut() );
 }
 
+bool ScProcess::running()
+{
+    return state() == QProcess::Running;
+}
+
 void ScProcess::toggleRunning()
 {
     switch(state()) {
@@ -153,8 +158,6 @@ void ScProcess::startLanguage (void)
     if(!configFile.isEmpty())
         sclangArguments << "-l" << configFile;
     sclangArguments << "-i" << "scqt";
-    if(standalone)
-        sclangArguments << "-a";
 
     if(!workingDirectory.isEmpty())
         setWorkingDirectory(workingDirectory);
